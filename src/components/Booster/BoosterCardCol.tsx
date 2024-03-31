3f2495ba205bb87f78c0980fa0c2833d0bab7b45
Mateusz <mati.tokarski@gmail.com>
2024-03-31 20:29:04 +0200

    Change display of multifaced cards

--- src/components/Booster/BoosterCardCol.tsx
+++ src/components/Booster/BoosterCardCol.tsx
@@ -1,28 +1,43 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 
 import { BoosterCard } from '../../state';
 
 const BoosterCardCol: React.FC<{ card: BoosterCard }> = ({ card }) => {
-  const imgUrlList = card.imgUrlList ?? [];
+  const [imgUrlIndex, setImgUrlIndex] = useState(0);
+  const imgUrlListLength = card.imgUrlList ? card.imgUrlList.length : 0;
+  const incrementImgUrlIndex = () => {
+    const indexNew = (imgUrlIndex + imgUrlListLength + 1) % imgUrlListLength;
+
+    setImgUrlIndex(indexNew);
+  };
+
+  const imgUrl = card.imgUrlList ? card.imgUrlList[imgUrlIndex] : null;
 
   return (
     <Col>
       <Card style={{ width: '12rem', minHeight: '24rem' }}>
-        {imgUrlList.map((url) => (
-          <Card.Img key={url} variant="top" src={url} alt={card.cardName} />
-        ))}
+        {imgUrl ? <Card.Img variant="top" src={imgUrl} alt={card.cardName} /> : <></>}
         <Card.Body>
           <Card.Title>{card.cardName}</Card.Title>
-          {card.dataUrl ? (
-            <Card.Text>
-              <a href={card.dataUrl} target="_blank">
-                Show on Gatherer
-              </a>
-            </Card.Text>
-          ) : (
-            <></>
-          )}
+          <Card.Text>
+            {imgUrlListLength > 1 ? (
+              <Button variant="link" onClick={incrementImgUrlIndex}>
+                Reverse
+              </Button>
+            ) : (
+              <></>
+            )}
+            {card.dataUrl ? (
+              <Button variant="link" href={card.dataUrl} target="_blank">
+                Card data
+              </Button>
+            ) : (
+              <></>
+            )}
+          </Card.Text>
         </Card.Body>
       </Card>
     </Col>
